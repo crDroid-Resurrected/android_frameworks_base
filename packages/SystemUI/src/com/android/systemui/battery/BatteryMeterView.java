45b859b0717f7f766a2bbfaed773362314baa05b
Ivy <78825510+Lost-Entrepreneur439@users.noreply.github.com>
2025-02-05 19:50:57 -0330

    Reapply "BatteryMeterView: Hide icon on battery-less devices"
    This reverts commit eb57437d219f761a8da3b0e119160fa1022a6b67.

--- packages/SystemUI/src/com/android/systemui/battery/BatteryMeterView.java
+++ packages/SystemUI/src/com/android/systemui/battery/BatteryMeterView.java
@@ -93,6 +93,7 @@ public class BatteryMeterView extends LinearLayout implements DarkReceiver {
     private int mShowPercentMode = MODE_DEFAULT;
     private String mEstimateText = null;
     private boolean mCharging;
+    private boolean mPresent = true;
     private boolean mIsOverheated;
     private boolean mDisplayShieldEnabled;
     // Error state where we know nothing about the current battery state
@@ -334,6 +335,10 @@ public class BatteryMeterView extends LinearLayout implements DarkReceiver {
         mBatteryEstimateFetcher = fetcher;
     }
 
+    void setBatteryPresence(boolean isPresent) {
+        mPresent = isPresent;
+    }
+
     void setDisplayShieldEnabled(boolean displayShieldEnabled) {
         mDisplayShieldEnabled = displayShieldEnabled;
     }
@@ -352,7 +357,7 @@ public class BatteryMeterView extends LinearLayout implements DarkReceiver {
 
     private void setPercentTextAtCurrentLevel() {
         String text = NumberFormat.getPercentInstance().format(mLevel / 100f);
-
+        
         mEstimateText = null;
 
         if (mBatteryEstimateFetcher != null && mShowPercentMode == MODE_ESTIMATE && !mCharging) {
@@ -482,7 +487,7 @@ public class BatteryMeterView extends LinearLayout implements DarkReceiver {
         mBatteryStateUnknown = isUnknown;
         updateContentDescription();
 
-        if (mBatteryStateUnknown) {
+        if (mBatteryStateUnknown && mPresent) {
             mBatteryIconView.setImageDrawable(getUnknownStateDrawable());
         } else {
             updateDrawable();
@@ -554,6 +559,9 @@ public class BatteryMeterView extends LinearLayout implements DarkReceiver {
     }
 
     private void updateDrawable() {
+        if (!mPresent) {
+            return;
+        }
 
         switch (mBatteryStyle) {
             case BATTERY_STYLE_PORTRAIT:
@@ -632,6 +640,7 @@ public class BatteryMeterView extends LinearLayout implements DarkReceiver {
         pw.println("    mBatteryStateUnknown: " + mBatteryStateUnknown);
         pw.println("    mLevel: " + mLevel);
         pw.println("    mMode: " + mShowPercentMode);
+        pw.println("    mPresent: " + mPresent);
     }
 
     @VisibleForTesting
@@ -657,3 +666,4 @@ public class BatteryMeterView extends LinearLayout implements DarkReceiver {
         mCallbacks.remove(callbacks);
     }
 }
+
